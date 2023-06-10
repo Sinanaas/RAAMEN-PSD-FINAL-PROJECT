31c4b547e53b8a7487a7e9f6fe7158c78cda6edc
Sinanaas <pudge842@gmail.com>
2023-06-10 17:49:47 +0700

    Third Commit

--- RAAMEN-PSD-FINAL-PROJECT/Factory/CartFactory.cs
+++ RAAMEN-PSD-FINAL-PROJECT/Factory/CartFactory.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace RAAMEN_PSD_FINAL_PROJECT.Controller
+namespace RAAMEN_PSD_FINAL_PROJECT.Factory
 {
-    public class CartFactory
-    {
 
-    }
 }
